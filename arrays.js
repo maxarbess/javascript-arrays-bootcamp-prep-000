e4860b3d09f8807c71583fd94dc9497324fa6603
Max Arbess <maxarbess1@gmail.com>
2019-01-28 22:53:43 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -32,5 +32,8 @@ var chocolateBars = [
     array.pop()
     return array
   }
-
+function removeElementFromEndOfArray(array)  {
+  array.slice(0,array.length-1)
+  return array.slice(0,array.length-1)
+}
   
